6c8f59f36dd07552740902cc16b6f75010c35e8e
AYAN <ayanbiwas184@gmail.com>
2020-10-14 03:48:38 +0530

    router configured

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container,Row,Col } from 'react-bootstrap';
+
+class HomePage extends React.Component {
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col>This is is the Home Page and This coding style should be followed</Col>
+                </Row>
+            </Container>
+
+        )
+    }
+}
+
+
+export default HomePage;
