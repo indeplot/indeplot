c8a06837df207a726266745731c5b96cb64df5f3
Femi Akinyemi <akinfemi46@gmail.com>
2020-10-14 22:00:43 +0100

    Implement the Footer Component for the Homepage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
+import {  HomePage } from './pages'
 
-import {
-  HomePage
-} from './pages'
 
 function App() {
   return (
     <Router>
       <Route path='/' exact component={HomePage} />
+      
     </Router>
 
   );
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Container,Row,Col } from 'react-bootstrap';
+import Footer from './Footer';
 
 class HomePage extends React.Component {
     render() {
         return (
-            <Container>
+           <div>
                 <Row>
                     <Col>This is is the Home Page and This coding style should be followed</Col>
                 </Row>
-            </Container>
-
+                 <Footer/>
+                 </div>
         )
     }
 }
