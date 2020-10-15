298f66746fc2aaa16722d3a4c4f0032f9263a8ed
Femi Akinyemi <akinfemi46@gmail.com>
2020-10-15 11:04:05 +0100

    Updated the Footer Components

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Row,Col } from 'react-bootstrap';
-import Footer from './Footer';
+import Footer from '../../Components/Footer';
 
 class HomePage extends React.Component {
     render() {
