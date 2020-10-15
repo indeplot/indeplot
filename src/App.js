c642704d10c9a9fb5cdd1efa2ba6db356dcbc03c
Femi Akinyemi <akinfemi46@gmail.com>
2020-10-15 12:30:02 +0100

    Implement the Navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from '../src/Assets/Images/logo.svg';
 import './App.css';
 
 import { BrowserRouter as Router, Route} from 'react-router-dom';
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Row,Col } from 'react-bootstrap';
 import Footer from '../../Components/Footer';
+import Navbar from '../../Components/Navbar';
 
 class HomePage extends React.Component {
     render() {
         return (
            <div>
+               <Navbar/>
                 <Row>
                     <Col>This is is the Home Page and This coding style should be followed</Col>
                 </Row>
