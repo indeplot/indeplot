439296c1de5bcc8d8f61829c8f739902c14f1a15
Femi Akinyemi <akinfemi46@gmail.com>
2020-10-16 13:05:56 +0100

    Updated the Navbar

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Row,Col } from 'react-bootstrap';
 import Footer from '../../Components/Footer';
-import Navbar from '../../Components/Navbar';
+import Navbar from '../../Components/Navbarr';
 
 class HomePage extends React.Component {
     render() {
