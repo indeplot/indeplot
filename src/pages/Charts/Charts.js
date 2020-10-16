357ef7d96678ef4f8c9869762b987aca8ed232c9
Varun N S <varun.klose@gmail.com>
2020-10-16 18:34:29 +0530

    [dev] Charts component added

---
+++ src/pages/Charts/Charts.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import { LineChart } from '../../Components/chart-picker/LineChart';
+import { BarChart } from '../../Components/chart-picker/BarChart';
+
+export default class Charts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.chartReference = React.createRef();
+        this.chartSelector = this.chartSelector.bind(this)
+    }
+
+    chartSelector = () => {
+
+        if (this.props.chartType === "Line") {
+            return <LineChart labels={this.props.labels} title={this.props.title} data={this.props.data} options={{}} />
+        }
+        if (this.props.chartType === "Bar") {
+            return <BarChart labels={this.props.labels} title={this.props.title} data={this.props.data} options={{}} />
+        }
+        return <LineChart labels={this.props.labels} title={this.props.title} data={this.props.data} options={{}} />
+    }
+
+    render() {
+        return (
+            <div>
+                {this.chartSelector()}
+            </div>
+        )
+    }
+}
+
+
+Charts.propTypes = {
+    chartType: PropTypes.string,
+    labels: PropTypes.array,
+    title: PropTypes.string,
+    data: PropTypes.array,
+    options: PropTypes.any
+};
