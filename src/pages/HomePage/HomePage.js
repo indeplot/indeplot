da89ecba93558eac1cc23f1b232245bd6cfd922a
Sooraj s <52352285+Sooraj-s-98@users.noreply.github.com>
2020-10-17 11:31:42 +0530

    mobile view corrected

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -15,23 +15,23 @@ class HomePage extends React.Component {
         }
     }
     render() {
-        return (
+           return (
 
-            <div>
+              <div>
                 <Navbar />
-                <Row>
-                    <Col>This is is the Home Page and This coding style should be followed</Col>
-                </Row>
-                <div style={{ display: "flex" }}>
-                    <div style={{ flex: 1 }}>
-                        <Charts chartType="Line" labels={this.state.labels} title="Sample 1" data={this.state.data} options={{}} />
-                    </div>
-                    <div style={{ flex: 1 }}>
+              <div className="container">
+                 <div className="row align-items-start">
+                     <Col>This is is the Home Page and This coding style should be followed</Col>
+                     <div className="col-12 col-md m-1" style={{ display: "flex" }}>
+                         <Charts chartType="Line" labels={this.state.labels} title="Sample 1" data={this.state.data} options={{}} />
+                     </div>
+                     <div className="col-12 col-md m-1">
                         <Charts chartType="Bar" labels={this.state.labels} title="Sample 2" data={this.state.data} options={{}} />
-                    </div>
-                </div>
-                <Footer />
-            </div>
+                     </div>
+                  </div>
+                 </div>
+               </div>
+ 
         )
     }
 }
