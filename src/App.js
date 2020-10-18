5c635ad26573cef7ce14c3716571f49ec166745b
dudakp <palodudak@gmail.com>
2020-10-18 15:38:53 +0200

    code style

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { HomePage } from './pages';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import {  HomePage } from './pages'
 import CodeOfConduct from './pages/CodeofConduct/CodeOfConduct';
+
 
 function App() {
   return (
+   
     <Router>
       <Route path='/' exact component={HomePage} />
       <Route path='/codeOfConduct' exact component={CodeOfConduct} />
     </Router>
+    
   );
 }
 
