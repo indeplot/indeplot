e6976e7d2c20e12cc45cb419bc82e734b3b15f01
Alikali Ojonugwa <alikali.ojonugwajustice@gmail.com>
2020-10-19 11:17:09 +0100

    Add Splashscreen

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -5,40 +5,54 @@ import Navbar from '../../Components/Navbarr';
 import ChartSelector from '../../Components/ChartSelector';
 import CodeEditor from '../../Components/CodeEditor';
 import Equation from '../../Components/Equation/Equation';
+import SplashScreen from '../../Components/SplashScreen';
 
 class HomePage extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			data: [65, 59, 80, 81, 56],
+			labels: ['SampleX1', 'SampleX2', 'SampleX3', 'SampleX4'],
+			showSplashScreen: true,
+		};
+  }
+  
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({showSplashScreen: false})
+    }, 2000);
+  }
 
-    constructor() {
-        super()
-        this.state = {
-            data: [65, 59, 80, 81, 56],
-            labels: ['SampleX1', 'SampleX2', 'SampleX3', 'SampleX4']
-        }
-    }
+	render() {
+		const { data, labels, showSplashScreen } = this.state;
 
-    render() {
-        const { data, labels } = this.state;
-        return (
-            <div>
-                <Navbar />
-                <Container fluid className="mb-4">
-                    <Row>
-                        <Col>This is is the Home Page and This coding style should be followed</Col>
-                    </Row>
-                </Container>
-                <ChartSelector data={data} labels={labels} />
-                <div style={{ marginBottom: '16px', border: '1px solid #eee', borderRadius: '8px', padding: '8px' }} className="col-sm-12">
-                    <CodeEditor />
-                </div>
+		if (showSplashScreen) {
+			return <SplashScreen />;
+		}
 
-                <div>
-                    <Equation/>
-                </div>
-                <Footer />
-            </div>
-        );
-    }
+		return (
+			<div>
+				<Navbar />
+				<Container fluid className="mb-4">
+					<Row>
+						<Col>This is is the Home Page and This coding style should be followed</Col>
+					</Row>
+				</Container>
+				<ChartSelector data={data} labels={labels} />
+				<div
+					style={{ marginBottom: '16px', border: '1px solid #eee', borderRadius: '8px', padding: '8px' }}
+					className="col-sm-12"
+				>
+					<CodeEditor />
+				</div>
+
+				<div>
+					<Equation />
+				</div>
+				<Footer />
+			</div>
+		);
+	}
 }
-
 
 export default HomePage;
