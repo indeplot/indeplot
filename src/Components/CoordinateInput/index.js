2139fec20464e273873fe5af300055aa8fb536a8
Missy Maloney <mmaloney@healthstarInfo.com>
2020-10-20 19:39:56 -0400

    revised format of input area to conform with other code

--- src/Components/CoordinateInput/index.js
+++ src/Components/CoordinateInput/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Col, Container, Form, Row, Button } from 'react-bootstrap';
+import { Container, Form, Button } from 'react-bootstrap';
 
 
 export default function CoordinateInput( props ) {
@@ -14,35 +14,28 @@ export default function CoordinateInput( props ) {
     }
     
     return (
-        <Container className="mt-2 mb-5">
+        <Container fluid className="m-3">
+            <h4>Add your own coordinates.</h4>
             <Form>
-                <Row><h3>Add your own coordinates.</h3></Row>
-                <Row className="align-items-end">
-                    <Col>
-                        <p>Enter an X coordinate:</p>
-                        <input
-                            name="xCoord"
-                            type="number"
-                            value={xCoord}
-                            onChange={ e => setXCoord(e.target.value)}
-                        />
-                    </Col>
-                    <Col>
-                        <p>Enter a Y coordinate:</p>
-                        <input
-                            name="yCoord"
-                            type="number"
-                            value={yCoord}
-                            onChange={ e => setYCoord(e.target.value)}
-
-                        />
-                    </Col>
-                    <Col xs={6}>
-                        <div className="align-middle"><Button onClick={setCoords}>Render</Button></div>
-                        
-                    </Col>
-                </Row>
-            </Form>
+                <Form.Group>
+                    <Form.Label>Enter an X coordinate:</Form.Label>
+                    <Form.Control 
+                        type="number"
+                        value={xCoord}
+                        onChange={ e => setXCoord(e.target.value)}
+                    >
+                    </Form.Control>
+                    <Form.Label>Enter a Y coordinate:</Form.Label>
+                    <Form.Control
+                        name="yCoord"
+                        type="number"
+                        value={yCoord}
+                        onChange={ e => setYCoord(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
+                <Button onClick={setCoords}>Render</Button>   
+            </Form>       
         </Container>
         
 
