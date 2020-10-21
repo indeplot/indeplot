a1a9ebea03791df04ca3ec0b0458230fa4577dfe
Missy Maloney <mmaloney@healthstarInfo.com>
2020-10-20 20:35:01 -0400

    aligned formatting with other components

--- src/Components/CoordinateInput/index.js
+++ src/Components/CoordinateInput/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Form, Button } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 
 
 export default function CoordinateInput( props ) {
@@ -14,29 +14,39 @@ export default function CoordinateInput( props ) {
     }
     
     return (
-        <Container fluid className="m-3">
-            <h4>Add your own coordinates.</h4>
+        <div className="d-flex flex-column form-group col-sm-12">
+            <div className="d-flex flex-row"> 
+                    <h1> X and Y</h1>
+                    <Button style={{margin: '10px', padding: '8px' }} className="btn btn-primary" onClick={setCoords}>Render</Button> 
+                </div>
             <Form>
-                <Form.Group>
-                    <Form.Label>Enter an X coordinate:</Form.Label>
-                    <Form.Control 
-                        type="number"
-                        value={xCoord}
-                        onChange={ e => setXCoord(e.target.value)}
-                    >
-                    </Form.Control>
-                    <Form.Label>Enter a Y coordinate:</Form.Label>
-                    <Form.Control
-                        name="yCoord"
-                        type="number"
-                        value={yCoord}
-                        onChange={ e => setYCoord(e.target.value)}
-                    >
-                    </Form.Control>
-                </Form.Group>
-                <Button onClick={setCoords}>Render</Button>   
-            </Form>       
-        </Container>
+                <Form.Row className="justify-content-center mb-3">
+                        <Form.Control 
+                            type="number"
+                            className="mr-sm-2 col-sm-2"
+                            style={{width:"25%",margin:"10px", textAlign:"center"}}
+                            value={xCoord}
+                            onChange={ e => setXCoord(e.target.value)}
+                            custom
+                            size="med"
+                            placeholder="X"
+                        >
+                        </Form.Control>
+
+                        <Form.Control
+                            className="mr-sm-2 col-sm-2"
+                            style={{width:"25%", margin:"10px", textAlign:"center"}}
+                            type="number"
+                            value={yCoord}
+                            onChange={ e => setYCoord(e.target.value)}
+                            custom
+                            size="med"
+                            placeholder="Y"
+                        >
+                        </Form.Control>
+                </Form.Row>
+            </Form>  
+        </div>     
         
 
     );
