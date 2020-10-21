16a64810cbafbc65905c573fc4f8b2f6f2e5c653
Thilaga <rthilagavathi305@gmail.com>
2020-10-21 14:46:38 +0530

    code cleanup

--- src/Components/chart-picker/HorizontalBarChart.js
+++ src/Components/chart-picker/HorizontalBarChart.js
@@ -3,21 +3,6 @@ import PropTypes from 'prop-types';
 
 import {HorizontalBar} from 'react-chartjs-2';
 
-const data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'My First dataset',
-      backgroundColor: 'colorVal',
-      borderColor: 'rgba(255,99,132,1)',
-      borderWidth: 1,
-      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-      hoverBorderColor: 'rgba(255,99,132,1)',
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-  ]
-};
-
 export function HorizontalBarChart(props){
   function generateGraph() {
     const colorVal = `rgba(${props.color.r},${props.color.g},${props.color.b},${props.color.a})`;
@@ -38,7 +23,6 @@ export function HorizontalBarChart(props){
   }
     return (
       <div>
-        <h2>Horizontal Bar Example</h2>
         <HorizontalBar data={generateGraph} />
       </div>
     );
